c2803c2007d22b8d08bf13292140f7fc6b9288e5
KostaSenatov <kostyasenatov@gmail.com>
2022-08-25 13:45:31 +0700

    .

--- src/js/module.js
+++ src/js/module.js
@@ -219,11 +219,21 @@ $(document).ready(function() {
 
 
 
+$(document).ready(function() {
+	function toggleInput(){
+		if(this.value){
+			$(this).parent('.input-wrapper').addClass('active');
+		}else{
+			$(this).parent('.input-wrapper').removeClass('active');
+		}
+	}
+	$('.form-item-anim__input').each(toggleInput);
+	$('.form-item-anim__input').on('input',toggleInput);
+})
 
 
-
-
-
-
-
-
+$(document).ready(function(){
+	$('img.lazyloadImages').lazyload({
+		effect: "fadeIn",
+	});
+});
